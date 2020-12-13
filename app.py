b51bd075c8f591fdab0a7452ee94dc0ed0da0488
Amiay Narayan <amiayn@iitbhilai.ac.in>
2020-12-13 11:05:27 +0530

    Complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify, make_response
 from werkzeug.utils import secure_filename
 from flask_pymongo import PyMongo
 import os
@@ -9,20 +9,27 @@ import uuid
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import urllib
+from authlib.integrations.flask_client import OAuth
 
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
-UPLOAD_FOLDER = './static/uploads/images'
+#UPLOAD_FOLDER = './static/uploads/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 # Sensisive informations have been removed so this will not work, instead I have provided dummy variables
-db_pass = urllib.parse.quote("nottheactualpassword")
-db_username = "notactualusername:"       # these are intentionally made vague before uploading on github
+db_pass = urllib.parse.quote("thisisnotmyactualserveradminname")
+db_username = "thisisnotmyactualpassword"       # these are intentionally made vague before uploading on github
 
-app.config["MONGO_URI"] = "mongodb+srv://"+db_username+db_pass+"@cluster0.wxsoe.mongodb.net/api_database"
-app.config["SECRET_KEY"] = "thisisnottheactualsecret"
+app.config["MONGO_URI"] = "mongodb+srv://"+db_username+db_pass+"@cluster0.wxsoe.mongodb.net/notactualnameofdatabase"
+app.config["SECRET_KEY"] = "nottheactualsecret"
 mongo = PyMongo(app)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key=b"justafakesecret"
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key=b"notactualkeyonceagain"
+
 
 limiter = Limiter(
     app,
@@ -36,7 +43,16 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = None
         print("this is what firefox sends back :",request.headers )
-        #print(request.headers["X-Access-Token"])
+        try:
+
+            print("from the token_requeried: ",request.headers["X-Access-Token"])
+        except:
+            pass
+        try:
+            token = request.form['x-access-token'][2:-1]
+            print("token received through form is: ", token)
+        except:
+            token = None
         if "X-Access-Token" in request.headers:
             token = request.headers['X-Access-Token']
             #print("token was successfully received from the request")
@@ -71,10 +87,12 @@ def login():
         else:
             public_id = user_requesting["public_id"]
             token = jwt.encode({"public_id":public_id, "name": request.form["user_name"], "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
-            #response = redirect(url_for("upload"))
-            #response.headers['X-JWT-TOKEN'] = token.decode("UTF-8")
+
+            response = make_response(render_template("upload.html",jwt=token ,message="Login Successful",instruction="click on upload to upload an image"))
+            response.headers.set('x-access-token' , token.decode("UTF-8"))
             #return response
-            return jsonify({"token": token.decode("UTF-8")})
+            #return jsonify({"token": token.decode("UTF-8")})
+            return response
             #return redirect(url_for("upload"))
 
     message = "Sign In to Upload."
@@ -93,7 +111,7 @@ def signupme():
         print(user["name"] , user["password"])
         if len(user["name"] )>3 and len(user["password"])>3:
             print("credentials are fine.. adding to the data base")
-            flash("created new user")
+            flash("Created new user")
             mongo.db.users1.insert(user)
             return redirect(url_for("login"))
         else:
@@ -110,7 +128,7 @@ def signupme():
 @limiter.limit("5/minute")      # 5 per minute 
 @token_required 
 def upload(current_user):
-    print(current_user, " is the info with you")
+#def upload():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -123,11 +141,11 @@ def upload(current_user):
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             print("the file name is : ", filename)
-            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             mongo.save_file(filename, file)
             mongo.db.pictures.insert({'public_id':current_user['public_id'], 'name':current_user['name'], 'filename':filename})
-            flash("Upload successfull for file with name : ", filename)
-            return redirect(url_for('success', filename=filename))
+            #response = redirect(url_for('success', filename=filename))
+            #response.headers['x-access-token'] = request.form['x-access-token'][2:-1]   # creates, but does not pass
+            return redirect(url_for("success", filename = filename, current_user_id=current_user['public_id']))
         flash("Only IMG, JPEG, JPG, PNG are allowed, Try Again")
     else:
         print("get requierst made")
@@ -136,13 +154,32 @@ def upload(current_user):
         return render_template("upload.html", message = message, instruction = instruction)
 @app.route("/succeed")
 @limiter.limit("5/minute")
-@token_required
-def success(current_user):
-    print("the file name asked is ", current_user)
-    print("this was the file name :", request.args['filename'])
-    filename = request.args['filename'] 
-    #filename = os.path.join(app.config['UPLOAD_FOLDER'],filename)
-    return f'<h1>{filename} </h1><img src={mongo.send_file(filename)}>'
+def success():
+    filename = request.args['filename']     # passed as message, from successful result
+    current_user_id = request.args['current_user_id']
+    pictures = mongo.db.pictures.find_one({"public_id":current_user_id, "filename": filename})
+    if not pictures:
+        return f'<h1>Image {filename} not found in the database</h1>', 404
+    print("Pictures found in the data base", pictures)
+    return f'''
+    <h1>{filename} </h1>
+    <div>
+    <div class="xzoom-container">
+    <img src="{url_for('file', filename=pictures['filename'])}" class="xzoom" xriginal="./images>
+    </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+    console.log("hi")
+    </script>
+    '''
+@limiter.limit("5/minute")
+@app.route("/uploaded/<filename>")
+def file(filename):
+    return mongo.send_file(filename)
+
+# create one route for the admin to manage the database, if time left
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test_api_rate.py
+++ test_api_rate.py
@@ -9,7 +9,7 @@ except:
 class FlaskTest(unittest.TestCase):
 
     # Check for respose 200
-    api_url = "http://localhost:5000"
+    api_url = "https://sheltered-lake-41348.herokuapp.com/"
 
     def test_index(self):
         response = requests.get(FlaskTest.api_url)
@@ -17,16 +17,17 @@ class FlaskTest(unittest.TestCase):
 
     def test_signup(self):
 
-        url = "http://localhost:5000/"
+        url = "https://sheltered-lake-41348.herokuapp.com/"
 
-        payload={'user_name': 'newuser',
-        'passwd': 'newpass'}
+        payload={'user_name': 'noob',
+        'passwd': 'noob'}
         headers = {
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
         self.assertEqual(response.status_code, 200)
-        token = response.text
+        jwt = response.headers['x-access-token']
+        '''
         jwt = ""
         for i in token:
             if i == '{' or i=='}' or i=='\n' or i=='"' or i==" ":
@@ -34,9 +35,10 @@ class FlaskTest(unittest.TestCase):
             else:
                 jwt +=i
         jwt = jwt.split(":")[-1]    
-        #print(jwt)
-        url = "http://localhost:5000/upload"
-        filereader = open('/home/tesla/Pictures/Wallpapers/bitmap.png','rb')
+        print("the jwt is :", jwt)
+        '''
+        url =url+ "upload"
+        filereader = open('test_image.png','rb')
         payload={}
         files=[
           ('file',('bitmap.png', filereader,'image/png'))
